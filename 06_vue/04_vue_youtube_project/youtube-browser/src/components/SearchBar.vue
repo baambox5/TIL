233bdf8a17ea2ef1855d303f53e139b3a57ae7d0
baambox5 <baamboxo@gmail.com>
2019-11-11 16:02:08 +0900

    191111 vue_youtube commit

---
+++ 06_vue/04_vue_youtube_project/youtube-browser/src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" @change="onInput">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+    methods: {
+      onInput(e) {
+        this.$emit('inputChange', e.target.value)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  input {
+    width: 75%;
+  }
+
+  div {
+    text-align: center;
+    margin: 20px;
+  }
+</style>
